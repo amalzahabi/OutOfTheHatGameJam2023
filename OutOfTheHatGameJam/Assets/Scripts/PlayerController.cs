f003f91413f362889505d22fd890b01bdfb0fa01
ChakaMoonga <71871929+ChakaMoonga@users.noreply.github.com>
2023-11-02 23:07:55 -0500

    General Prototyping Update

--- OutOfTheHatGameJam/Assets/Scripts/PlayerController.cs
+++ OutOfTheHatGameJam/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
   
     public float moveSpeed = 5f;
-   
+    public float jumpForce = 10f;
+    
     private Rigidbody2D rb;
    
     private void Start()
@@ -17,6 +18,13 @@ public class PlayerController : MonoBehaviour
     private void Update() { 
         float moveInput = Input.GetAxis("Horizontal"); 
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        
+        
+        if (Input.GetButtonDown("Jump")) // Check if the jump button is pressed
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse); // Apply the jump force
+        }
+    }
     }
    
-}
+
